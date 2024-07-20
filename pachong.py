a36c25fc65156aced6b9c00d04487157416bde3a
cheesestudio <52149451+cheesestudio@users.noreply.github.com>
2024-07-20 18:34:40 +0800

    `1

--- pachong.py
+++ pachong.py
@@ -7,9 +7,9 @@ header = {
 url = "https://team-up-website-sveltekit.vercel.app/leaderboard/1023107249532571648/Pool%E5%8F%B0%E7%90%83%F0%9F%98%8B/overall_player/all"
 
 if __name__ == "__main__":
-    resp = requests.get(url=url, headers=header,verify=False)  
+    resp = requests.get(url=url, headers=header)  
     resp = resp.content.decode(requests.utils.get_encodings_from_content(resp.text)[0])
     rul = re.compile("<div.*?<p.*?>(?P<rank>.*?)<.*?button.*?<p>(?P<name>.*?)</p.*?<p.*?>(?P<score>.*?)</p>.*?</div>")
     with open(file="1.txt",mode="w", encoding="utf-8") as f:
         for i in re.finditer(rul, resp):\
-            f.write(f"{i.group('rank'):<10} {i.group('name'):<20}\t{i.group('score')}\n")
+            f.write(f"{i.group('rank'):<10} {i.group('name'):<20}    {i.group('score')}\n")
