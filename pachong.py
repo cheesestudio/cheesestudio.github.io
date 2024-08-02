40192344cf23b1dc3124c4cf312dac46ef959a7d
cheesestudio <52149451+cheesestudio@users.noreply.github.com>
2024-08-02 15:20:20 +0800

    1

--- pachong.py
+++ pachong.py
@@ -4,12 +4,12 @@ import re
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
 }
-url = "https://team-up-website-sveltekit.vercel.app/leaderboard/1023107249532571648/Pool%E5%8F%B0%E7%90%83%F0%9F%98%8B/overall_player/all"
+url = "https://teamupdiscord.com/leaderboard/1023107249532571648/Pool%E5%8F%B0%E7%90%83%F0%9F%98%8B/overall_player/all"
 
 if __name__ == "__main__":
     resp = requests.get(url=url, headers=header)  
     resp = resp.content.decode(requests.utils.get_encodings_from_content(resp.text)[0])
-    rul = re.compile("<div.*?<p.*?>(?P<rank>[0-9]*).*?<.*?button.*?<p>(?P<name>.*?)</p.*?<p.*?>(?P<score>.*?)</p>.*?</div>")
+    rul = re.compile("<div.*?<p.*?>(?P<rank>[0-9]*).*?</p>.*?<button.*?<p.*?>(?P<name>.*?)</p>.*?</button>.*?<p.*?>(?P<elo>.*?)</p>.*?</div>")
     with open(file="1.txt",mode="w", encoding="utf-8") as f:
-        for i in re.finditer(rul, resp):\
-            f.write(f"{i.group('rank'):}.{i.group('name'):<3} {i.group('score')}\n")
+        for i in re.finditer(rul, resp):
+            f.write(f"{i.group('rank'):}.{i.group('name'):<3} {i.group('elo')}\n")
